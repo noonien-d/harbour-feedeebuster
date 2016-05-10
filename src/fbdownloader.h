87656073077b779e04996f66b69741dbc6c8c1b3
Ferdinand Stehle <development@kondorgulasch.de>
2016-05-10 20:47:01 +0200

    set proper license headers

--- src/fbdownloader.h
+++ src/fbdownloader.h
@@ -1,3 +1,17 @@
+/* feedeebuster - a feed and podcast application for Sailfish OS.
+ * Copyright (C) 2016  Ferdinand Stehle
+ *
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software Foundation;
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>. */
+
 #ifndef FBDOWNLOADER_H
 #define FBDOWNLOADER_H
 
