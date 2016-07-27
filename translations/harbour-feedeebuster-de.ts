e67aa1f849157f129ca1b8718d86bf602adef799
Ferdinand Stehle <development@kondorgulasch.de>
2016-07-27 22:58:26 +0200

    add german translation

--- translations/harbour-feedeebuster-de.ts
+++ translations/harbour-feedeebuster-de.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="de_DE">
 <context>
     <name>ConfigPage</name>
     <message>
         <location filename="../qml/pages/ConfigPage.qml" line="25"/>
         <source>Configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>Einstellungen</translation>
     </message>
     <message>
         <location filename="../qml/pages/ConfigPage.qml" line="30"/>
         <source>Show Icons</source>
-        <translation type="unfinished"></translation>
+        <translation>Symbole anzeigen</translation>
     </message>
     <message>
         <location filename="../qml/pages/ConfigPage.qml" line="38"/>
         <source>Show Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Benachrichtigungen anzeigen</translation>
     </message>
     <message>
         <location filename="../qml/pages/ConfigPage.qml" line="46"/>
         <source>Background refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktualisierung im Hintergrund</translation>
     </message>
 </context>
 <context>
@@ -29,7 +29,7 @@
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="45"/>
         <source>unread</source>
-        <translation type="unfinished"></translation>
+        <translation>ungelesen</translation>
     </message>
 </context>
 <context>
@@ -37,22 +37,22 @@
     <message>
         <location filename="../qml/pages/FeedPage.qml" line="27"/>
         <source>Subscriptions</source>
-        <translation type="unfinished"></translation>
+        <translation>Abonnements</translation>
     </message>
     <message>
         <location filename="../qml/pages/FeedPage.qml" line="31"/>
         <source>Add feed</source>
-        <translation type="unfinished"></translation>
+        <translation>Hinzufügen</translation>
     </message>
     <message>
         <location filename="../qml/pages/FeedPage.qml" line="69"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Entfernen</translation>
     </message>
     <message>
         <location filename="../qml/pages/FeedPage.qml" line="73"/>
         <source>Copy Url</source>
-        <translation type="unfinished"></translation>
+        <translation>Url kopieren</translation>
     </message>
 </context>
 <context>
@@ -60,22 +60,22 @@
     <message>
         <location filename="../qml/pages/ItemPage.qml" line="42"/>
         <source>Download</source>
-        <translation type="unfinished"></translation>
+        <translation>Herunterladen</translation>
     </message>
     <message>
         <location filename="../qml/pages/ItemPage.qml" line="83"/>
         <source>Download failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Herunterladen fehlgeschlagen</translation>
     </message>
     <message>
         <location filename="../qml/pages/ItemPage.qml" line="161"/>
         <source>view full content</source>
-        <translation type="unfinished"></translation>
+        <translation>Volltext</translation>
     </message>
     <message>
         <location filename="../qml/pages/ItemPage.qml" line="231"/>
         <source>Website</source>
-        <translation type="unfinished"></translation>
+        <translation>Website</translation>
     </message>
 </context>
 <context>
@@ -83,22 +83,22 @@
     <message>
         <location filename="../qml/pages/ListPage.qml" line="33"/>
         <source>Configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>Einstellungen</translation>
     </message>
     <message>
         <location filename="../qml/pages/ListPage.qml" line="37"/>
         <source>Manage subscriptions</source>
-        <translation type="unfinished"></translation>
+        <translation>Abonnements</translation>
     </message>
     <message>
         <location filename="../qml/pages/ListPage.qml" line="41"/>
         <source>Reload all</source>
-        <translation type="unfinished"></translation>
+        <translation>Alle aktualisieren</translation>
     </message>
     <message>
         <location filename="../qml/pages/ListPage.qml" line="50"/>
         <source>Items</source>
-        <translation type="unfinished"></translation>
+        <translation>Elemente</translation>
     </message>
 </context>
 </TS>
